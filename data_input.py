7ddde8658755c309d8c5039f04af390b589f5b41
Andrew Uvarov <43270753+Uvarovka@users.noreply.github.com>
2023-01-23 11:06:01 +0900

    исправил вывод для ввода аргументов

--- data_input.py
+++ data_input.py
@@ -2,10 +2,10 @@ import pickle
 
 
 def create_dict(fio, dob, ser_pass, num_pass):
-    my_dict = {'ФИО в формате (Иванов Иван Иванович)': fio, 'Дата рождения в формате (xx.xx.xxxx)': dob, 'Серия паспорта': ser_pass, 'Номер паспорта': num_pass}
+    my_dict = {'fio': fio, 'dob': dob, 'ser_pass': ser_pass, 'num_pass': num_pass}
     return my_dict
 
-my_input_dict = create_dict(input("Enter fio: "), input("Enter dob: "), input("Enter ser_pass: "), input("Enter num_pass: "))
+my_input_dict = create_dict(input("ФИО в формате (Иванов Иван Иванович): "), input("Дата рождения в формате (xx.xx.xxxx): "), input("Серия паспорта: "), input("Номер паспорта: "))
 
 with open("dict.pickle", "wb") as handle:
     pickle.dump(my_input_dict, handle)
