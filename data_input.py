1b9136aad2fd80eb1491b352fe89d439195dd043
Andrew Uvarov <43270753+Uvarovka@users.noreply.github.com>
2023-01-23 10:57:55 +0900

    fixed temp arg's output

--- data_input.py
+++ data_input.py
@@ -2,7 +2,7 @@ import pickle
 
 
 def create_dict(fio, dob, ser_pass, num_pass):
-    my_dict = {'fio': fio, 'dob': dob, 'ser_pass': ser_pass, 'num_pass': num_pass}
+    my_dict = {'ФИО в формате (Иванов Иван Иванович)': fio, 'Дата рождения в формате (xx.xx.xxxx)': dob, 'Серия паспорта': ser_pass, 'Номер паспорта': num_pass}
     return my_dict
 
 my_input_dict = create_dict(input("Enter fio: "), input("Enter dob: "), input("Enter ser_pass: "), input("Enter num_pass: "))
